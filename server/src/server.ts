423fbe5a39814dc12d90272cd4a13cec648c02c5
Jacob Norlin Andersson <jacob.norlinandersson@gmail.com>
2020-09-27 16:21:18 +0200

    Add cors filter to response

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,98 +1,127 @@
-import http = require('http')
-import { RouteHandler, Match, Request, Route } from './route'
-import { HttpMethod, RouteLike, Map } from '@common/types'
-import { hasOwnProperty } from '@common/util'
+import http = require('http');
+import { RouteHandler, Match, Request, Route } from './route';
+import { HttpMethod, RouteLike, Map } from '@common/types';
+import { hasOwnProperty } from '@common/util';
 
-type FindMethods<T extends RouteLike<any, any>> = T['method']
-type FindRouteByMethod<T extends RouteLike<any, any>, M extends HttpMethod> =
-    T extends { method: M } ? T : never
-type FindRoute<T extends RouteLike<any, any>, M extends HttpMethod, P extends string> =
-    T extends { method: M, path: P } ? T : never
+type FindMethods<T extends RouteLike<any, any>> = T['method'];
+type FindRouteByMethod<
+    T extends RouteLike<any, any>,
+    M extends HttpMethod
+> = T extends { method: M } ? T : never;
+type FindRoute<
+    T extends RouteLike<any, any>,
+    M extends HttpMethod,
+    P extends string
+> = T extends { method: M; path: P } ? T : never;
 
-function tryParseJson<T extends string, D = undefined>(value: T, def?: D): object | D {
+function tryParseJson<T extends string, D = undefined>(
+    value: T,
+    def?: D
+): object | D {
     try {
-        return JSON.parse(value)
+        return JSON.parse(value);
     } catch (e) {
-        return def!
+        return def!;
     }
 }
 
 export class Server<T extends RouteLike<any, any>> {
-    protected readonly port: string
-    protected readonly host: string
-    protected readonly routes: Array<Route<any>>
+    protected readonly port: string;
+    protected readonly host: string;
+    protected readonly routes: Array<Route<any>>;
 
     constructor(port: string, host?: string) {
-        this.port = port
-        this.host = host || '127.0.0.1'
-        this.routes = [] as any
+        this.port = port;
+        this.host = host || '127.0.0.1';
+        this.routes = [] as any;
     }
 
-    public route<M extends FindMethods<T>, P extends FindRouteByMethod<T, M>['path']>(method: M, path: P, handler: RouteHandler<FindRoute<T, M, P>>): Server<T> {
-        this.routes.push(new Route(method, path, handler))
-        return this as any
+    public route<
+        M extends FindMethods<T>,
+        P extends FindRouteByMethod<T, M>['path']
+    >(
+        method: M,
+        path: P,
+        handler: RouteHandler<FindRoute<T, M, P>>
+    ): Server<T> {
+        this.routes.push(new Route(method, path, handler));
+        return this as any;
     }
 
     public listen(): void {
         const server = http.createServer((httpRequest, httpResponse) => {
-            httpRequest.setEncoding('utf8')
+            httpRequest.setEncoding('utf8');
+            // Set CORS headers
+            httpResponse.setHeader('Access-Control-Allow-Origin', '*');
+            httpResponse.setHeader('Access-Control-Request-Method', '*');
+            httpResponse.setHeader(
+                'Access-Control-Allow-Methods',
+                'OPTIONS, GET'
+            );
+            httpResponse.setHeader('Access-Control-Allow-Headers', '*');
 
-            const method = httpRequest.method ?? 'GET'
-            const url = httpRequest.url ?? '/'
-            let match: Match<any> | undefined
+            if (httpRequest.method === 'OPTIONS') {
+                httpResponse.writeHead(200);
+                httpResponse.end();
+                return;
+            }
+
+            const method = httpRequest.method ?? 'GET';
+            const url = httpRequest.url ?? '/';
+            let match: Match<any> | undefined;
 
             for (const route of this.routes) {
-                const m = route.matches(method, url)
+                const m = route.matches(method, url);
                 if (m) {
-                    match = m
-                    break
+                    match = m;
+                    break;
                 }
             }
 
             if (!match) {
-                httpResponse.writeHead(404)
-                httpResponse.end()
-                return
+                httpResponse.writeHead(404);
+                httpResponse.end();
+                return;
             }
 
-            let body = ''
-            httpRequest.on('data', chunk => {
-                body += chunk
-            })
+            let body = '';
+            httpRequest.on('data', (chunk) => {
+                body += chunk;
+            });
             httpRequest.on('end', () => {
                 const request: Request<any> = {
                     body: tryParseJson(body, {}),
                     method,
                     path: httpRequest.url,
-                    routeParams: match?.params,
-                }
+                    routeParams: match?.params
+                };
 
-                Promise.resolve(match!.handler(request)).then(response => {
-                    let responseStatus = 200
-                    let responseBody = ''
-                    let responseHeaders: Map<string> = {}
+                Promise.resolve(match!.handler(request)).then((response) => {
+                    let responseStatus = 200;
+                    let responseBody = '';
+                    let responseHeaders: Map<string> = {};
 
                     if (hasOwnProperty(response, 'body', 'status')) {
-                        responseStatus = response.status
-                        responseBody = response.body
+                        responseStatus = response.status;
+                        responseBody = response.body;
                     } else if (typeof response === 'object') {
-                        responseBody = JSON.stringify(response)
-                        responseHeaders['Content-Type'] = 'application/json'
+                        responseBody = JSON.stringify(response);
+                        responseHeaders['Content-Type'] = 'application/json';
                     } else {
-                        responseBody = String(response)
+                        responseBody = String(response);
                     }
 
-                    httpResponse.writeHead(responseStatus, responseHeaders)
-                    httpResponse.end(responseBody)
-                })
-            })
-        })
+                    httpResponse.writeHead(responseStatus, responseHeaders);
+                    httpResponse.end(responseBody);
+                });
+            });
+        });
 
-        const HTTP_HOST: string = process.env['HTTP_HOST'] || '127.0.0.1'
-        const HTTP_PORT: string = process.env['HTTP_PORT'] || '8080'
+        const HTTP_HOST: string = process.env['HTTP_HOST'] || '127.0.0.1';
+        const HTTP_PORT: string = process.env['HTTP_PORT'] || '8080';
 
-        console.debug(`Listening on ${HTTP_HOST}:${HTTP_PORT}`)
+        console.debug(`Listening on ${HTTP_HOST}:${HTTP_PORT}`);
 
-        server.listen(parseInt(HTTP_PORT), HTTP_HOST)
+        server.listen(parseInt(HTTP_PORT), HTTP_HOST);
     }
 }
